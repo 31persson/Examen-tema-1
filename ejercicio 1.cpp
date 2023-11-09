7286429f8bd5dbbad67f69bdc2682551255b1fdf
31persson <jperscas@myuax.com>
2023-11-09 12:00:59 +0100

    ejercicio 1

--- ejercicio 1.cpp
+++ ejercicio 1.cpp
@@ -6,7 +6,7 @@ int suma(int a, int b) {
 
 int main() {
     int num1 = 45;
-    int num2 = 23;
+    int num2 = 21;
     int resultado = suma(num1, num2);
 
     std::cout << "La suma de " << num1 << " y " << num2 << " es " << resultado << std::endl;
