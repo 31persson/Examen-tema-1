81e5067d945f3fef5ae2a5cedd865789d077e7aa
31persson <jperscas@myuax.com>
2023-11-09 12:00:13 +0100

    ejercicio 1

---
+++ ejercicio 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int suma(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1 = 45;
+    int num2 = 23;
+    int resultado = suma(num1, num2);
+
+    std::cout << "La suma de " << num1 << " y " << num2 << " es " << resultado << std::endl;
+
+    return 0;
+}
