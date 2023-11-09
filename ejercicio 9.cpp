4ad51f6df7b9cd959bc8de7cd04b231e0badbf59
31persson <jperscas@myuax.com>
2023-11-09 12:43:31 +0100

    ejercicio 9

--- ejercicio 9.cpp
+++ ejercicio 9.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
 #include <string>
-#include <stdexcept>
+#include <vector>  // Agregar esta línea para incluir la biblioteca vector
 
-// Definición de la excepción personalizada
-class MateriaYaRegistradaException : public std::exception {
-public:
-    MateriaYaRegistradaException(const std::string& materia) : mensaje("La materia '" + materia + "' ya ha sido registrada.") {}
-
-    const char* what() const throw() {
-        return mensaje.c_str();
-    }
-
-private:
-    std::string mensaje;
-};
-
-// Clase Estudiante
 class Estudiante {
 public:
     std::string nombre;
     int edad;
     std::string grado;
-    std::vector<std::string> materias;
+    std::vector<std::string> materias;  // Utilizar vector para almacenar materias
 
     Estudiante(const std::string& n, int e, const std::string& g) : nombre(n), edad(e), grado(g) {}
 
@@ -29,7 +15,7 @@ public:
     void registrarMateria(const std::string& materia) {
         for (const std::string& m : materias) {
             if (m == materia) {
-                throw MateriaYaRegistradaException(materia); // Lanzar la excepción personalizada
+                throw MateriaYaRegistradaException(materia);
             }
         }
         materias.push_back(materia);
@@ -39,16 +25,14 @@ public:
 int main() {
     Estudiante estudiante1("Juan Perez", 18, "12º grado");
 
-    // Registrar algunas materias
     try {
         estudiante1.registrarMateria("Matemáticas");
         estudiante1.registrarMateria("Ciencias");
-        estudiante1.registrarMateria("Matemáticas"); // Intentar registrar la misma materia nuevamente
+        estudiante1.registrarMateria("Matemáticas");
     } catch (const MateriaYaRegistradaException& e) {
         std::cerr << "Excepción capturada: " << e.what() << std::endl;
     }
 
-    // Mostrar las materias registradas
     std::cout << "Materias registradas:" << std::endl;
     for (const std::string& materia : estudiante1.materias) {
         std::cout << "- " << materia << std::endl;
