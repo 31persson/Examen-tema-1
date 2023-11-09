68a331f67e18e5b1e32e57d6af41460c77fcbf1b
31persson <jperscas@myuax.com>
2023-11-09 12:12:50 +0100

    ejercicio 3

--- ejercicio 3.cpp
+++ ejercicio 3.cpp
@@ -10,14 +10,14 @@ int main() {
     int num1 = 5;
     int num2 = 7;
 
-    std::cout << "Antes del intercambio:" << std::endl;
-    std::cout << "num1 = " << num1 << ", num2 = " << num2 << std::endl;
+    std::cout << "Antes:" << std::endl;
+    std::cout << "numero 1 = " << num1 << ", numero 2 = " << num2 << std::endl;
 
     // Llamar a la función para intercambiar los valores
     intercambiar(&num1, &num2);
 
-    std::cout << "Después del intercambio:" << std::endl;
-    std::cout << "num1 = " << num1 << ", num2 = " << num2 << std::endl;
+    std::cout << "Despues:" << std::endl;
+    std::cout << "numero 1 = " << num1 << ", numero 2 = " << num2 << std::endl;
 
     return 0;
 }
