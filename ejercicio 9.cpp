ad81245388005362a8fe6b084574ab46181cdc1a
31persson <jperscas@myuax.com>
2023-11-09 12:45:25 +0100

    ejercicio 9

--- ejercicio 9.cpp
+++ ejercicio 9.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <string>
-#include <vector>  // Agregar esta línea para incluir la biblioteca vector
+#include <vector>
+
+// Declaración de la excepción personalizada
+class MateriaYaRegistradaException : public std::exception {
+public:
+    MateriaYaRegistradaException(const std::string& materia) : mensaje("La materia '" + materia + "' ya ha sido registrada.") {}
+
+    const char* what() const throw() {
+        return mensaje.c_str();
+    }
+
+private:
+    std::string mensaje;
+};
 
 class Estudiante {
 public:
     std::string nombre;
     int edad;
     std::string grado;
-    std::vector<std::string> materias;  // Utilizar vector para almacenar materias
+    std::vector<std::string> materias;
 
     Estudiante(const std::string& n, int e, const std::string& g) : nombre(n), edad(e), grado(g) {}
 
@@ -23,14 +36,14 @@ public:
 };
 
 int main() {
-    Estudiante estudiante1("Juan Perez", 18, "12º grado");
+    Estudiante estudiante1("Juan Perez", 18, "Ingenieria");
 
     try {
-        estudiante1.registrarMateria("Matemáticas");
+        estudiante1.registrarMateria("Matematicas");
         estudiante1.registrarMateria("Ciencias");
-        estudiante1.registrarMateria("Matemáticas");
+        estudiante1.registrarMateria("Matematicas");
     } catch (const MateriaYaRegistradaException& e) {
-        std::cerr << "Excepción capturada: " << e.what() << std::endl;
+        std::cerr << "Error: " << e.what() << std::endl;
     }
 
     std::cout << "Materias registradas:" << std::endl;
