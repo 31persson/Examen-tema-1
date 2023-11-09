ae823aa11ced4e6f098ad2f0ef6cc6579235352b
31persson <jperscas@myuax.com>
2023-11-09 12:23:39 +0100

    ejercicio 4

--- ejercicio 2.cpp
+++ ejercicio 2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Estudiante {
 public:
@@ -7,6 +8,7 @@ public:
     std::string nombre;
     int edad;
     std::string grado;
+    std::vector<std::string> materias;  // Vector para almacenar las materias registradas
 
     // Constructor para inicializar las propiedades
     Estudiante(std::string n, int e, std::string g) : nombre(n), edad(e), grado(g) {}
@@ -17,14 +19,32 @@ public:
         std::cout << "Edad: " << edad << " años" << std::endl;
         std::cout << "Grado: " << grado << std::endl;
     }
+
+    // Función miembro para registrar una materia
+    void registrar_materia(const std::string& materia) {
+        materias.push_back(materia);
+    }
+
+    // Función miembro para mostrar las materias registradas
+    void mostrar_materias() {
+        std::cout << "Materias registradas:" << std::endl;
+        for (const std::string& materia : materias) {
+            std::cout << "- " << materia << std::endl;
+        }
+    }
 };
 
 int main() {
-    // Crear un objeto de la clase Estudiante
     Estudiante estudiante1("Juan Perez", 18, "Informatica");
 
-    // Llamar a la función para mostrar la información del estudiante
+    // Registrar algunas materias
+    estudiante1.registrar_materia("Programacion");
+    estudiante1.registrar_materia("Matematicas");
+    estudiante1.registrar_materia("Fisica");
+
+    // Mostrar la información del estudiante y las materias registradas
     estudiante1.mostrar_info();
+    estudiante1.mostrar_materias();
 
     return 0;
 }
